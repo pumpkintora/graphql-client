2265bea027da6f92f59f5d7429986060a429fdb1
pumpkintora <renkiat.gan@corpxervices.com>
2023-04-11 12:37:38 +0800

    browse  character component

--- graphql-client/src/app/browse/browse.component.ts
+++ graphql-client/src/app/browse/browse.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Character, CharactersService } from '../characters.service'
 
 @Component({
-  selector: 'app-browse',
-  templateUrl: './browse.component.html',
-  styleUrls: ['./browse.component.css']
+    selector: 'app-browse',
+    templateUrl: './browse.component.html',
+    styleUrls: ['./browse.component.css'],
 })
-export class BrowseComponent {
+export class BrowseComponent implements OnInit {
+    offset: number = 0
+    count: number = 0
+    characters: Character[] = []
 
+    constructor(private charactersService: CharactersService) {}
+
+    async ngOnInit(): Promise<void> {
+        await this.updateCharacters()
+    }
+
+    async updateCharacters() {
+        const result = await this.charactersService.getCharacters(this.offset)
+        this.count = result.count
+        this.characters = result.characters
+    }
+
+    showPrevious() {
+        return this.offset > 0
+    }
+
+    showNext() {
+        return this.offset + 10 < this.count
+    }
+
+    async onPrevious() {
+        this.offset -= 10
+        await this.updateCharacters()
+    }
+
+    async onNext() {
+        this.offset += 10
+        await this.updateCharacters()
+    }
 }
